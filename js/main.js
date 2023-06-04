27b46a81fa63205c8ee9503bada2169b46a2a643
Stevens Contreras <scopar@tutanota.com>
2023-06-03 22:04:31 -0500

    Carga de tareas a focus

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ class Task {
     this._id = ++Task.count;
     this._description = description;
     this._listName = listName;
-    this._position = null; // Nueva propiedad para almacenar la posición
+    this._position = null;
+    this._dueDate = null;
   }
 
   get id() {
@@ -35,32 +36,50 @@ class Task {
   set position(newPosition) {
     this._position = newPosition;
   }
+
+  get dueDate() {
+    return this._dueDate;
+  }
+
+  set dueDate(newDate) {
+    this._dueDate = newDate;
+  }
+
 }
 
 let toDoListTasks = [];
-  
+
 // Obtener la fecha actual
-  const currentDate = new Date();
+const currentDate = new Date();
 
 // Obtener el día de la semana en formato largo
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+const options = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+};
 const formattedDate = currentDate.toLocaleDateString(undefined, options);
 
-  // Obtener el elemento div por su ID
-  const currentDateDiv = document.getElementById("current-date");
+// Obtener el elemento div por su ID
+const currentDateDiv = document.getElementById("current-date");
 
-  // Establecer el contenido del div como la fecha formateada
-  currentDateDiv.textContent = formattedDate;
+// Establecer el contenido del div como la fecha formateada
+currentDateDiv.textContent = formattedDate;
 
 // Obtener elementos del DOM
 const completedTaskList = document.querySelector('ul[name="completed-task"]');
-const addItemButton = document.querySelector('.add-new-task-button');
+const addItemButton = document.querySelector(".add-new-task-button");
 const newTaskTemplate = document.getElementById("blueListItem-newTask");
 const taskLoaderButton = document.getElementById("taskloader");
 const blueToDoListDetails = document.querySelector("#blue-to-do-list-details");
 const redToDoListDetails = document.querySelector("#red-to-do-list-details");
-const orangeToDoListDetails = document.querySelector("#orange-to-do-list-details");
-const yellowToDoListDetails = document.querySelector("#yellow-to-do-list-details");
+const orangeToDoListDetails = document.querySelector(
+  "#orange-to-do-list-details"
+);
+const yellowToDoListDetails = document.querySelector(
+  "#yellow-to-do-list-details"
+);
 
 // Event listeners
 addItemButton.addEventListener("click", addNewTaskToDoList);
@@ -68,15 +87,17 @@ taskLoaderButton.addEventListener("click", loadItems);
 
 function addTaskToList(task, listElement) {
   listElement.open = true;
-  listElement.querySelector('ul').appendChild(task);
+  listElement.querySelector("ul").appendChild(task);
 }
 
 function findTaskById(taskId) {
-  return toDoListTasks.find(task => task.id === taskId);
+  return toDoListTasks.find((task) => task.id === taskId);
 }
 
 function findTaskByIdAndListName(id, listName) {
-  return toDoListTasks.find(task => task.id === id && task.listName === listName);
+  return toDoListTasks.find(
+    (task) => task.id === id && task.listName === listName
+  );
 }
 
 function updateTaskDescription(taskId, newDescription) {
@@ -96,7 +117,7 @@ function updateTaskPositions(listName) {
 }
 
 function deleteTaskById(taskId) {
-  toDoListTasks = toDoListTasks.filter(task => task.id !== taskId);
+  toDoListTasks = toDoListTasks.filter((task) => task.id !== taskId);
 }
 
 function validateInput(inputValue) {
@@ -104,14 +125,14 @@ function validateInput(inputValue) {
 }
 
 function toggleDropdown(event) {
-  const dropdown = event.currentTarget.closest('.custom-dropdown');
-  const summary = dropdown.querySelector('summary');
+  const dropdown = event.currentTarget.closest(".custom-dropdown");
+  const summary = dropdown.querySelector("summary");
   summary.click();
 }
 
 // Manejo de clasificaciones
 function handleClassification(event) {
-  const li = event.currentTarget.closest('.task-elementList');
+  const li = event.currentTarget.closest(".task-elementList");
   const currentButton = li.querySelector(".current-button button");
   const buttonsDropdownList = li.querySelectorAll(".dropdown-list button");
   const idValue = parseInt(li.id);
@@ -123,29 +144,29 @@ function handleClassification(event) {
 
   const classValues = {
     "red-to-do-list": [
-      { class: 'changeClass-redButton', value: 'red-to-do-list' },
-      { class: 'changeClass-orangeButton', value: 'orange-to-do-list' },
-      { class: 'changeClass-yellowButton', value: 'yellow-to-do-list' },
-      { class: 'changeClass-blueButton', value: 'blue-to-do-list' }
+      { class: "changeClass-redButton", value: "red-to-do-list" },
+      { class: "changeClass-orangeButton", value: "orange-to-do-list" },
+      { class: "changeClass-yellowButton", value: "yellow-to-do-list" },
+      { class: "changeClass-blueButton", value: "blue-to-do-list" },
     ],
     "orange-to-do-list": [
-      { class: 'changeClass-orangeButton', value: 'orange-to-do-list' },
-      { class: 'changeClass-redButton', value: 'red-to-do-list' },
-      { class: 'changeClass-yellowButton', value: 'yellow-to-do-list' },
-      { class: 'changeClass-blueButton', value: 'blue-to-do-list' }
+      { class: "changeClass-orangeButton", value: "orange-to-do-list" },
+      { class: "changeClass-redButton", value: "red-to-do-list" },
+      { class: "changeClass-yellowButton", value: "yellow-to-do-list" },
+      { class: "changeClass-blueButton", value: "blue-to-do-list" },
     ],
     "yellow-to-do-list": [
-      { class: 'changeClass-yellowButton', value: 'yellow-to-do-list' },
-      { class: 'changeClass-redButton', value: 'red-to-do-list' },
-      { class: 'changeClass-orangeButton', value: 'orange-to-do-list' },
-      { class: 'changeClass-blueButton', value: 'blue-to-do-list' }
+      { class: "changeClass-yellowButton", value: "yellow-to-do-list" },
+      { class: "changeClass-redButton", value: "red-to-do-list" },
+      { class: "changeClass-orangeButton", value: "orange-to-do-list" },
+      { class: "changeClass-blueButton", value: "blue-to-do-list" },
     ],
     "blue-to-do-list": [
-      { class: 'changeClass-blueButton', value: 'blue-to-do-list' },
-      { class: 'changeClass-redButton', value: 'red-to-do-list' },
-      { class: 'changeClass-orangeButton', value: 'orange-to-do-list' },
-      { class: 'changeClass-yellowButton', value: 'yellow-to-do-list' }
-    ]
+      { class: "changeClass-blueButton", value: "blue-to-do-list" },
+      { class: "changeClass-redButton", value: "red-to-do-list" },
+      { class: "changeClass-orangeButton", value: "orange-to-do-list" },
+      { class: "changeClass-yellowButton", value: "yellow-to-do-list" },
+    ],
   };
 
   const selectedClassValues = classValues[buttonValue];
@@ -156,35 +177,32 @@ function handleClassification(event) {
   currentButton.value = buttonValue;
 
   buttonsDropdownList.forEach((button, index) => {
-    const element = selectedClassValues[index+1];
+    const element = selectedClassValues[index + 1];
     button.classList.remove(button.classList.value);
     button.classList.add(element.class);
     button.value = element.value;
   });
 
-  const targetDetails = document.getElementById(buttonValue + '-details');
-  
+  const targetDetails = document.getElementById(buttonValue + "-details");
+
   // Agregar la tarea a la lista que corresponde
   addTaskToList(li, targetDetails);
   updateTaskPositions(foundTask.listName);
   updateTaskPositions(actualList);
 
-
   li.querySelector(".custom-dropdown").open = false;
 
-  checkListLength (actualToDoList[0].childNodes.length, actualToDoListDetails);
+  checkListLength(actualToDoList[0].childNodes.length, actualToDoListDetails);
 }
 
 function checkListLength(currentToDoList, currentDetailsElement) {
-  
   if (currentToDoList === 0) {
     currentDetailsElement.open = false;
   }
-
 }
 
 function editListItem(event) {
-  const li = event.currentTarget.closest('.task-elementList');
+  const li = event.currentTarget.closest(".task-elementList");
   const inputElement = li.querySelector(".taskDescription");
   const taskId = parseInt(li.id);
 
@@ -204,7 +222,11 @@ function editListItem(event) {
         // Si el usuario deja el campo en blanco y presiona Enter, elimina la tarea
         li.remove();
         deleteTaskById(taskId);
-        checkListLength (actualToDoList[0].childNodes.length, actualToDoListDetails);
+        updateTaskPositions(actualList)
+        checkListLength(
+          actualToDoList[0].childNodes.length,
+          actualToDoListDetails
+        );
       } else if (newDescription !== inputElement.defaultValue) {
         // Si el usuario hace modificaciones en la descripción de la tarea
         updateTaskDescription(taskId, newDescription);
@@ -216,7 +238,7 @@ function editListItem(event) {
 }
 
 function upListItem(event) {
-  const listItem = event.target.closest('li');
+  const listItem = event.target.closest("li");
   const previousListItem = listItem.previousElementSibling;
 
   if (previousListItem) {
@@ -234,7 +256,7 @@ function addNewTaskToDoList(event) {
 
   // Si el input está vacío se sale da la función
   if (!validateInput(newTaskInput)) return;
-  
+
   const listName = "blue-to-do-list";
 
   // Crea una nueva instancia de la clase Task con la descripción y listName predeterminados
@@ -242,16 +264,21 @@ function addNewTaskToDoList(event) {
   toDoListTasks.push(newTask);
 
   // Clona el template y obtener la referencia al elemento de la nueva tarea
-  const newTaskListItem = newTaskTemplate.content.cloneNode(true).firstElementChild;
+  const newTaskListItem =
+    newTaskTemplate.content.cloneNode(true).firstElementChild;
 
   // Obtiene el elemento <input> dentro del elemento de la nueva tarea
   const inputElement = newTaskListItem.querySelector(".taskDescription");
 
   // Obtiene las referencias de los botones
-  const blueButton = newTaskListItem.querySelector('.changeClass-blueButton');
-  const redButton = newTaskListItem.querySelector('.changeClass-redButton');
-  const orangeButton = newTaskListItem.querySelector('.changeClass-orangeButton');
-  const yellowButton = newTaskListItem.querySelector('.changeClass-yellowButton');
+  const blueButton = newTaskListItem.querySelector(".changeClass-blueButton");
+  const redButton = newTaskListItem.querySelector(".changeClass-redButton");
+  const orangeButton = newTaskListItem.querySelector(
+    ".changeClass-orangeButton"
+  );
+  const yellowButton = newTaskListItem.querySelector(
+    ".changeClass-yellowButton"
+  );
   const upArrowButton = newTaskListItem.querySelector(".upArrow-button");
   const editItemButton = newTaskListItem.querySelector(".edit-item-button");
 
@@ -279,24 +306,33 @@ function addNewTaskToDoList(event) {
 }
 
 function loadItems() {
-  const listNames = ["red-to-do-list", "orange-to-do-list", "yellow-to-do-list"];
-  const focusOnSixList = document.getElementById('focusOn-sixList');
+  const listNames = [
+    "red-to-do-list",
+    "orange-to-do-list",
+    "yellow-to-do-list",
+  ];
+  const focusOnSixList = document.getElementById("focusOn-sixList");
 
   const template = document.getElementById("focusOn-elementsList");
 
   let loadedItems = 0;
 
   for (const listName of listNames) {
-    const tasks = toDoListTasks.filter(task => task.listName === listName);
+    const tasks = toDoListTasks.filter((task) => task.listName === listName);
     const sortedTasks = tasks.sort((a, b) => a.position - b.position);
 
     for (const task of sortedTasks) {
       const listItem = document.getElementById(task.id);
 
       if (listItem) {
-        const clonedTemplate = template.content.cloneNode(true).firstElementChild;
-        const descriptionElement = clonedTemplate.querySelector(".taskDescription-elementList");
-        const returnButton = clonedTemplate.querySelector(".return-task-button");
+        const clonedTemplate =
+          template.content.cloneNode(true).firstElementChild;
+        const descriptionElement = clonedTemplate.querySelector(
+          ".taskDescription-elementList"
+        );
+        const returnButton = clonedTemplate.querySelector(
+          ".return-task-button"
+        );
         const startButton = clonedTemplate.querySelector(".start-task-button");
 
         descriptionElement.textContent = task.description;
@@ -316,10 +352,10 @@ function loadItems() {
   }
 }
 
-function handleReturnTask (){
-  console.log("retorno")
+function handleReturnTask() {
+  console.log("retorno");
 }
 
-function handleStartTask(){
-  console.log("Empieza")
+function handleStartTask() {
+  console.log("Empieza");
 }
